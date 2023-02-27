b7afbebd4077694bfbb07c63ccaa538fe847404b
Alex <ajtravis2@gmail.com>
2023-02-27 16:57:26 -0500

    fixed state issues with cart

--- react-app/src/components/ShoppingCart/index.js
+++ react-app/src/components/ShoppingCart/index.js
@@ -11,7 +11,7 @@ export default function MyCart(){
     const dispatch = useDispatch()
     const cartItems = useSelector(state => state.cart.items)
     const order = useSelector(state => state.cart.order)
-
+    const cartItemsL= Object.values(cartItems)
 
     useEffect(() => {
         dispatch(thunkMyCart());
@@ -22,7 +22,7 @@ return (
    <div>
     <h2>Your Cart:</h2>
     <div>Total: {Math.floor(order?.amount * 100) / 100}</div>
-    {cartItems? cartItems?.map(item =>
+    {cartItemsL? cartItemsL?.map(item =>
         <div>
             <CartCard item={item} />
         </div>
