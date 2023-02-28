84c428a8411e1289f8d9b36aa58fef3d126b76af
Alex <ajtravis2@gmail.com>
2023-02-28 12:40:15 -0500

    new users can now add items to their cart

--- app/api/auth_routes.py
+++ app/api/auth_routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify, session, request
-from app.models import User, db
+from app.models import User, db, Order
 from app.forms import LoginForm
 from app.forms import SignUpForm
 from flask_login import current_user, login_user, logout_user, login_required
@@ -70,6 +70,14 @@ def sign_up():
             password=form.data['password']
         )
         db.session.add(user)
+        db.session.commit()
+
+        order = Order(
+            user_id=user.id,
+            amount=0.00,
+            closed=False
+        )
+        db.session.add(order)
         db.session.commit()
         login_user(user)
         return user.to_dict()
