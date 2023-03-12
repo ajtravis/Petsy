a836bd0cab82ead22f88d8b499a95ec2ced3828d
Alex <ajtravis2@gmail.com>
2023-03-11 21:24:10 -0500

    bugfix

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -4,5 +4,4 @@ from .cart_item import Cart_Item
 from .order import Order
 from .product import Product
 from .review import Review
-from .category import Category
 from .db import environment, SCHEMA
