a3090b77e10e55aee1d76b81b8f9ea5df7267c1c
Alex <ajtravis2@gmail.com>
2023-04-12 22:18:33 -0400

    error handling for reviews

--- app/forms/review_form.py
+++ app/forms/review_form.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField
-from wtforms.validators import DataRequired, ValidationError
+from wtforms.validators import DataRequired, ValidationError, Length
 
 class ReviewForm(FlaskForm):
-    review = StringField('review', validators=[DataRequired()])
+    review = StringField('review', validators=[DataRequired("please write a review"), Length(min=1, max=255, message="length must be between 1 and 255 characters")])
     rating = IntegerField('rating', validators=[DataRequired()])
